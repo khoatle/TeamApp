ea150adcaaf80c02f12cc87423d39909afb20197
Khoa <khoa@khoa-VirtualBox.(none)>
2012-11-24 15:52:21 -0600

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamApp::Application.config.secret_token = '0310896d0de97d220a72466ed7947bdb8c6af291f3e62bb001c30fba28039d5b8dc98b58f8e90007954507ab14510893a79bf5e9f4aa29f4c42aea15f9c4cdf6'
